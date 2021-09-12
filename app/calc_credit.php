02f6655d88419066abc50c65386f0351ebdae482
Sebastian Zimny <sebastian.zimny1@gmail.com>
2021-09-12 17:36:21 +0200

    Added security - login panel

--- app/calc_credit.php
+++ app/calc_credit.php
@@ -8,43 +8,50 @@ require_once dirname(__FILE__).'/../config.php';
 
 // 1. pobranie parametrów
 
-$x = $_REQUEST ['x'];
-$y = $_REQUEST ['y'];
-$operation = $_REQUEST ['op'];
+include _ROOT_PATH.'/app/security/check.php';
+
+function getParams(&$x,&$y,&$operation){
+	$x = isset($_REQUEST['x']) ? $_REQUEST['x'] : null;
+	$y = isset($_REQUEST['y']) ? $_REQUEST['y'] : null;
+	$operation = isset($_REQUEST['op']) ? $_REQUEST['op'] : null;	
+}
 
 // 2. walidacja parametrów z przygotowaniem zmiennych dla widoku
 
 // sprawdzenie, czy parametry zostały przekazane
-if ( ! (isset($x) && isset($y) && isset($operation))) {
-	//sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
-	$messages [] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
-}
+function validate(&$x,&$y,&$operation,&$messages){
+        if ( ! (isset($x) && isset($y) && isset($operation))) {
+                //sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
+                return false;
+        }
 
-// sprawdzenie, czy potrzebne wartości zostały przekazane
-if ( $x == "") {
-	$messages [] = 'Nie podano kwoty';
-}
-if ( $y == "") {
-	$messages [] = 'Nie podano długości spłaty';
-}
+        // sprawdzenie, czy potrzebne wartości zostały przekazane
+        if ( $x == "") {
+            $messages [] = 'Nie podano kwoty';
+        }
+        if ( $y == "") {
+            $messages [] = 'Nie podano długości spłaty';
+        }
 
-//nie ma sensu walidować dalej gdy brak parametrów
-if (empty( $messages )) {
-	
-	// sprawdzenie, czy $x i $y są liczbami całkowitymi
-	if (! is_numeric( $x )) {
-		$messages [] = 'Pierwsza wartość nie jest liczbą całkowitą';
-	}
-	
-	if (! is_numeric( $y )) {
-		$messages [] = 'Druga wartość nie jest liczbą całkowitą';
-	}	
+        //nie ma sensu walidować dalej gdy brak parametrów
+        if (count ( $messages ) != 0) return false;
 
+        // sprawdzenie, czy $x i $y są liczbami całkowitymi
+        if (! is_numeric( $x )) {
+            $messages [] = 'Pierwsza wartość nie jest liczbą całkowitą';
+        }
+
+        if (! is_numeric( $y )) {
+            $messages [] = 'Druga wartość nie jest liczbą całkowitą';
+        }	
+            
+        if (count ( $messages ) != 0) return false;
+	else return true;
 }
 
 // 3. wykonaj zadanie jeśli wszystko w porządku
-
-if (empty ( $messages )) { // gdy brak błędów
+function process(&$x,&$y,&$operation,&$messages,&$result){
+    global $role;
 	
 	//konwersja parametrów na int
 	$x = floatval($x);
@@ -53,8 +60,12 @@ if (empty ( $messages )) { // gdy brak błędów
 	//wykonanie operacji
 	switch ($operation) {
 		case '1%' :
+                    if ($role == 'admin'){
 			$result = ($x/$y)+($x/$y)*0.01;
                         $result = round($result,2);
+                    }else {
+				$messages [] = 'Tylko administrator może wybrać 1% !';
+			}
 			break;
 		case '2%' :
 			$result = ($x/$y)+($x/$y)*0.02;
@@ -79,6 +90,19 @@ if (empty ( $messages )) { // gdy brak błędów
 	}
 }
 
+
+//definicja zmiennych kontrolera
+$x = null;
+$y = null;
+$operation = null;
+$result = null;
+$messages = array();
+
+//pobierz parametry i wykonaj zadanie jeśli wszystko w porządku
+getParams($x,$y,$operation);
+if ( validate($x,$y,$operation,$messages) ) { // gdy brak błędów
+	process($x,$y,$operation,$messages,$result);
+}
 // 4. Wywołanie widoku z przekazaniem zmiennych
 // - zainicjowane zmienne ($messages,$x,$y,$operation,$result)
 //   będą dostępne w dołączonym skrypcie
--- app/calc_credit_view.php
+++ app/calc_credit_view.php
@@ -2,25 +2,36 @@
 <!DOCTYPE HTML>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
 <head>
-<meta charset="utf-8" />
-<title>Kalkulator</title>
+    <meta charset="utf-8" />
+    <title>Kalkulator</title>
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
 </head>
 <body>
 
-<form action="<?php print(_APP_URL);?>/app/calc_credit.php" method="post">
-	<label for="id_x">Kwota: </label>
-	<input id="id_x" type="text" name="x" value="<?php if(isset($x)) print($x); ?>" /><br />
-	<label for="id_op">Oprocentowanie: </label>
-	<select name="op">
-		<option value="1%">1%</option>
-		<option value="2%">2%</option>
-		<option value="3%">3%</option>
-		<option value="5%">5%</option>
-                <option value="10%">10%</option>
-	</select><br />
+<div style="width:90%; margin: 2em auto;">
+	<a href="<?php print(_APP_ROOT); ?>/app/inna_chroniona.php" class="pure-button">kolejna chroniona strona</a>
+	<a href="<?php print(_APP_ROOT); ?>/app/security/logout.php" class="pure-button pure-button-active">Wyloguj</a>
+</div>
+
+<div style="width:90%; margin: 2em auto;">    
+    
+<form action="<?php print(_APP_URL);?>/app/calc_credit.php" method="post" class="pure-form pure-form-stacked">
+        <legend>Kalkulator</legend>	
+        <fieldset>
+            <label for="id_x">Kwota: </label>
+            <input id="id_x" type="text" name="x" value="<?php if(isset($x)) print($x); ?>" /><br />
+            <label for="id_op">Oprocentowanie: </label>
+            <select name="op">
+                    <option value="1%">1%</option>
+                    <option value="2%">2%</option>
+                    <option value="3%">3%</option>
+                    <option value="5%">5%</option>
+                    <option value="10%">10%</option>
+	<</select><br />
 	<label for="id_y">Długość spłaty w miesiącach: </label>
 	<input id="id_y" type="text" name="y" value="<?php if(isset($y)) print($y); ?>" /><br />
-	<input type="submit" value="Oblicz" />
+	</fieldset>
+        <input type="submit" value="Oblicz" class="pure-button pure-button-primary" />
 </form>	
 
 <?php
@@ -41,6 +52,6 @@ if (isset($messages)) {
 <?php echo 'Twoja rata będzie wynosić: '.$result; ?>
 </div>
 <?php } ?>
-
+</div>
 </body>
 </html>
